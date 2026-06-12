cde491f129d807516d20f084ced96a37aab40108
Denoremitero <76872724+Denoremitero@users.noreply.github.com>
2026-06-12 20:11:14 -0300

    ultimos cambios antes de la build

--- My project/Assets/Scripts/Objects/PhraseButton.cs
+++ My project/Assets/Scripts/Objects/PhraseButton.cs
@@ -3,13 +3,30 @@ using UnityEngine;
 
 public class PhraseButton : MonoBehaviour
 {
-    public int phraseIndex;
+    public int phraseIndex  = 0;
     public string phraseText;
 
     [SerializeField] TextMeshProUGUI m_TextMeshPro;
+    [SerializeField] GameplayUIManager m_GameplayUIManager;
 
+    private void Start()
+    {
+        m_GameplayUIManager = FindFirstObjectByType<GameplayUIManager>();
+    }
     public void ChangePhraseText()
     {
-        m_TextMeshPro.text = phraseText;
+        if (m_TextMeshPro == null)
+        {
+            return;
+        }
+        else
+        {
+            m_TextMeshPro.text = phraseText;
+
+        }
+    }
+    public void OnClickButtonPhrase()
+    {
+        m_GameplayUIManager.ButtonPhraseCompleter(phraseIndex);
     }
 }
